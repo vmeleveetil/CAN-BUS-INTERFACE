eb3de2e691d17d838cf59476c26a0765c8c016a5
vmeleveetil <vedanth.meleveetil@generalfusion.com>
2022-05-16 15:01:10 -0700

    draft rev 0.0

--- main.py
+++ main.py
@@ -8,32 +8,45 @@ Created on Fri May 13 14:48:26 2022
 import can
 import time
 import os
+import queue
+from threading import Thread
 
-bInit = True
+os.system("sudo /sbin/ip link set can0 down")
+print('\n\rReset CAN Link')
+print('\n\rBring up CAN0....')
+os.system("sudo /sbin/ip link set can0 up type can bitrate 500000")
+time.sleep(0.1)	
+try:
+    bus = can.interface.Bus(channel='can0', bustype='socketcan')
+except OSError:
+    print('Cannot find PiCAN board.')
+    exit()
+print('Ready')
 
-while True:
-    if bool(bInit):
-        system("sudo /sbin/ip link set can0 down")
-        print('\n\rReset CAN Link')
-        print('\n\rBring up CAN0....')
-        os.system("sudo /sbin/ip link set can0 up type can bitrate 500000")
-        time.sleep(0.1)	
-        try:
-        	bus = can.interface.Bus(channel='can0', bustype='socketcan')
-        except OSError:
-        	print('Cannot find PiCAN board.')
-        	exit()
-        print('Ready')
-        bInit= False
+# CAN receive thread
+def can_rx_task():
+	while True:
+		message = bus.recv()
+		q.put(message)			# Put message into queue
 
+q = queue.Queue()	
+t = Thread(target = can_rx_task)	# Start receive thread
+t.start()
+
+# Main loop
 try:
 	while True:
-		message = bus.recv()	# Wait until a message is received.
-		
-		c = '{0:f} {1:x} {2:x} '.format(message.timestamp, message.arbitration_id, message.dlc)
-		s=''
-		for i in range(message.dlc ):
-			s +=  '{0:x} '.format(message.data[i])
-			  
-		print(' {}'.format(c+s))
+		if q.empty() != True:	# Check if there is a message in queue
+			message = q.get()
+			c = '{0:f} {1:x} {2:x} '.format(message.timestamp, message.arbitration_id, message.dlc)
+			s=''
+			for i in range(message.dlc ):
+				s +=  '{0:x} '.format(message.data[i])
+
+			print('\r {} qsize:{}       '.format((c+s),q.qsize()),end ='') # Print data and queue size on screen
+			
 	
+except KeyboardInterrupt:
+	#Catch keyboard interrupt
+	os.system("sudo /sbin/ip link set can0 down")
+	print('\n\rKeyboard interrtupt')	
